1b6e9a562bff0eea0100fb6ff27f508e62e9ac23
TGianoni <gianoni2@hotmail.com>
2025-05-13 09:52:09 -0300

    feat: ensure bookings are listed on bookings page

---
+++ app/bookings/page.tsx
@@ -0,0 +1,71 @@
+import BookingItem from "../_components/booking-item"
+import Header from "../_components/header"
+import { authOptions } from "../_lib/auth"
+import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+
+const Bookings = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session?.user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-2xl font-bold">Você não está logado</h1>
+      </div>
+    )
+  }
+  const confirmedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+  })
+  const concludedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        lt: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+  return (
+    <>
+      <Header />
+      <div className="space-y-3 p-5">
+        <h1 className="text-xl font-bold">Agendamentos</h1>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Confirmados
+        </h2>
+        {confirmedBookings.map((booking) => (
+          <BookingItem key={booking.id} booking={booking} />
+        ))}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Finalizados
+        </h2>
+        {concludedBookings.map((booking) => (
+          <BookingItem key={booking.id} booking={booking} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Bookings
--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,38 @@ import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 const Home = async () => {
   // chamar meu banco de dados
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session?.user as any)?.id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
   return (
     <div>
       <Header />
@@ -59,8 +82,17 @@ const Home = async () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
         {/* AGENDAMENTO */}
-        <BookingItem />
+        <div className="mt-6 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
